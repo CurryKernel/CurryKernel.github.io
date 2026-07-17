4348b967a270539a7e0121324abc354f61175335
CurryKernel <1289735075@qq.com>
2026-07-17 09:45:17 +0800

    feat: add blog list page with content collection

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    tags: z.array(z.string()).default([]),
+    description: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
